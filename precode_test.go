ef7c980b1660867a89c9fe3968a927bd82efdcf2
Sam <yablokov-s-a@mail.ru>
2024-02-06 19:30:53 +0300

    Тесты в отдельном файле. Создан тест на кол-во кафе

---
+++ precode_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=10", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	answer := responseRecorder.Body.String()
+	answerCount := len(strings.Split(answer, ","))
+
+	assert.Equal(t, totalCount, answerCount)
+}
