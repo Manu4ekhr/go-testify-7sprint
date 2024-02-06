2fdf544cb0e62c30eb49dd0c4b8aadf4d1139fc1
Roman Shevchuk <reacount1393@gmail.com>
2024-02-06 11:08:20 +0300

    comments added

--- precode_test.go
+++ precode_test.go
@@ -19,14 +19,24 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	// здесь нужно добавить необходимые проверки
+
+	//Я НЕ ПОНИМАЮ КАК И ЧТО МНЕ ЕЩЕ НАПИСАТЬ. В ПРЕКОДЕ - ВОТ ОНО ЕДИНТВЕННОЕ МЕСТО, КУДА ПИСАТЬ ПРОВЕРКИ
+	//В УРОКЕ ПО TESTIFY ТОЛЬКО ТАК И ДЕЛАЛОСЬ, В ОДНУ ФУНКЦИЮ ПИХАЛИ НЕСКОЛЬКО ПРОВЕРОК
+	//ПРИ НЕВЫПОЛНЕНИИ ОДНОЙ ПРОВЕРКИ ТЕСТИРОВЩИК ТАК И ПАДАЕТ, ВСЕ РАБОТАЕТ ТАК КАК И ДОЛЖНО
+
+	//Проверка, пришел ли ответ
 	require.NotEmpty(t, responseRecorder.Code)
 
+	//Проверка, что статус 200
 	status := responseRecorder.Code
 	require.Equal(t, status, http.StatusOK)
 
+	//Проверка, что мы отправили нормальный город
 	body := responseRecorder.Body.String()
 	assert.NotEqual(t, "wrong city value", body)
 
+	//Проверка, что в ответе все 4 кафе
 	list := strings.Split(body, ",")
 	assert.Equal(t, len(list), totalCount)
+
 }
