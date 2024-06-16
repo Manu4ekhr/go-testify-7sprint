6aa2d6f4d1435e23825a3df6445d0fd33ff94be5
ThAhriman <lishchenko91@mail.ru>
2024-06-16 19:19:55 +0300

    first commit

---
+++ main_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) { //Если в параметре count указано больше,
+	// чем есть всего, должны вернуться все доступные кафе.
+	totalCount := 4
+	// здесь нужно создать запрос к сервису
+
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
+	// проверка количества полученных в ответе кафе с их действительным количеством, при условии что count
+	// больше этого количества
+	body := responseRecorder.Body.String()
+
+	cafeList := strings.Split(body, ",")
+
+	cafeNumbers := len(cafeList)
+
+	assert.Equal(t, totalCount, cafeNumbers)
+
+}
+
+func TestMainHandlerWhenResponseBodyNotEmpty(t *testing.T) { //запрос сформирован корректно,
+	// сервис возвращает код ответа 200 и тело ответа не пустое
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//проверка кода ответа
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+
+	//проверка не пустое ли тело ответа
+	require.NotEmpty(t, responseRecorder.Body)
+
+}
+
+func TestMainHandlerWhenResponseCityNotSupported(t *testing.T) { // Город, который передаётся в параметре city,
+	// не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
+
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=rostov", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	city := req.URL.Query().Get("city")
+
+	assert.NotContains(t, cafeList, city)
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+
+}
