bf4d054194e05b9e445dfe54af47fe26324443c9
Shukhrat <164924657+SeanWalter1@users.noreply.github.com>
2024-06-23 17:04:00 +0300

    precode.go

--- precode.go
+++ precode.go
@@ -45,14 +45,3 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
     w.WriteHeader(http.StatusOK)
     w.Write([]byte(answer))
 }
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-    totalCount := 4
-    req := ... // здесь нужно создать запрос к сервису
-
-    responseRecorder := httptest.NewRecorder()
-    handler := http.HandlerFunc(mainHandle)
-    handler.ServeHTTP(responseRecorder, req)
-
-    // здесь нужно добавить необходимые проверки
-}
