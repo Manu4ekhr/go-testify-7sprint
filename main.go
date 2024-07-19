51487c42731526693b5ad5140352c5eed0c199a8
AleksandrTveritin <AleksandrTveritin@yandex.ru>
2024-07-19 22:41:59 +0300

    two

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -47,64 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerValidRequest(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-}
-
-func TestMainHandlerInvalidCity(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=2&city=paris", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
-}
-
-func TestMainHandlerCountMoreThanTotal(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.Equal(t, "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент", responseRecorder.Body.String())
-}
-
-func TestMainHandlerCountMissing(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?city=moscow", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Equal(t, "count missing", responseRecorder.Body.String())
-}
-
-func TestMainHandlerInvalidCount(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=abc&city=moscow", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Equal(t, "wrong count value", responseRecorder.Body.String())
 }
