56cdf1b5cb8aae099fb58a42d0536faa8508e115
Gena Lebedev <pattison.job@gmail.com>
2024-08-14 12:01:03 +0300

    second commit

--- precode_test.go
+++ precode_test.go
@@ -18,13 +18,13 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 	actualCity := req.URL.Query().Get("city")
 	for city, _ := range cafeList {
 		assert.Equal(t, city, actualCity)
 	}
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
-	require.Equal(t, responseRecorder.Code, http.StatusOK)
 	assert.NotEmpty(t, body)
 	assert.Len(t, list, totalCount)
 	// здесь нужно добавить необходимые проверки
