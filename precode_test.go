9c848bfa19116873254a1e4ab28139307d8ea57d
Gena Lebedev <pattison.job@gmail.com>
2024-08-16 20:53:14 +0300

    third commit

--- precode_test.go
+++ precode_test.go
@@ -10,22 +10,34 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.Equal(t, responseRecorder.Code, http.StatusOK)
-	actualCity := req.URL.Query().Get("city")
-	for city, _ := range cafeList {
-		assert.Equal(t, city, actualCity)
-	}
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+}
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
-	assert.NotEmpty(t, body)
 	assert.Len(t, list, totalCount)
-	// здесь нужно добавить необходимые проверки
+}
+func TestMainHandlerWhenNotEmpty(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	body := responseRecorder.Body.String()
+	assert.NotEmpty(t, body)
 }
