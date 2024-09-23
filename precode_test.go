c744f650e9f3fc7d858d5958a9cd9b2060e31e2f
Kovalev Evgeniy <kjone232323@yandex.ru>
2024-09-23 11:15:14 +0300

    Created second test TestMainHandlerWhenCityIsNotSupported.

--- precode_test.go
+++ precode_test.go
@@ -21,3 +21,16 @@ func TestMainHandlerWhenOkAndNotEmpty(t *testing.T) {
 	assert.NotEmpty(t, responseRecorder.Body.String())
 
 }
+
+func TestMainHandlerWhenCityIsNotSupported(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=rostov", nil) // Создаём запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//Проверяем что город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа..
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
+
+}
