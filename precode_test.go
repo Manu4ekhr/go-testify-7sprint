c4a376095f43168f8d167035b1e16ced54dcaf1b
Kovalev Evgeniy <kjone232323@yandex.ru>
2024-09-23 11:06:31 +0300

    Created first test TestMainHandlerWhenOkAndNotEmpty.

--- precode_test.go
+++ precode_test.go
@@ -4,15 +4,20 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	//totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // Создаём запрос к сервису
+func TestMainHandlerWhenOkAndNotEmpty(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil) // Создаём запрос к сервису
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	// здесь нужно добавить необходимые проверки
+	//Проверяем что запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое.
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
+	assert.NotEmpty(t, responseRecorder.Body.String())
+
 }
