ae77672804372475b1ef198afd10993ec3aaf88f
Kovalev Evgeniy <kjone232323@yandex.ru>
2024-09-23 11:32:20 +0300

    Created third test TestMainHandlerWhenMissingCount.

--- precode_test.go
+++ precode_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,5 +33,20 @@ func TestMainHandlerWhenCityIsNotSupported(t *testing.T) {
 	//Проверяем что город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа..
 	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
+
+}
+
+func TestMainHandlerWhenMissingCount(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // Создаём запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//Проверяем что если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе.
+	list := strings.Split(responseRecorder.Body.String(), ",")
+	assert.Len(t, list, totalCount)
+	assert.Equal(t, cafeList["moscow"], list)
 
 }
